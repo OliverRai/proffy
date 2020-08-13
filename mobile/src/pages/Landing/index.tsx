cebb0347949e37c050b6b65c27fb02ea967d8f67
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-08-13 12:44:24 -0300

    tab navegation

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, Text, TouchableOpacity} from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
@@ -10,38 +10,43 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
-function Landing(){
-    const {navigate}  = useNavigation();
+function Landing() {
+    const { navigate } = useNavigation();
 
-function handleNavigateToGiveClassesPage(){
-    navigate('GiveClasses');
-}
+    function handleNavigateToGiveClassesPage() {
+        navigate('GiveClasses');
+    }
+
+    function handleNavigateToStudyPages() {
+        navigate("Study");
+    }
 
     return (
-    <View style={styles.container}>
-        <Image source={landingImg} style={styles.banner}></Image>
-        <Text style={styles.title}>
-            Seja Bem vindo, {'\n'}
-            <Text style={styles.titleBold}>O que deseja fazer?</Text>
-        </Text>
-        <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
-                <Image source={studyIcon}></Image>
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner}></Image>
+            <Text style={styles.title}>
+                Seja Bem vindo, {'\n'}
+                <Text style={styles.titleBold}>O que deseja fazer?</Text>
+            </Text>
+            <View style={styles.buttonsContainer}>
 
-                <Text style={styles.buttonText}>Estudar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
-                <Image source={giveClassesIcon}></Image>
+                <TouchableOpacity onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon}></Image>
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
 
-                <Text style={styles.buttonText}>Dar Aulas</Text>
-            </TouchableOpacity>
+                <TouchableOpacity onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon}></Image>
+                    <Text style={styles.buttonText}>Dar Aulas</Text>
+                </TouchableOpacity>
+
+            </View>
+            <Text style={styles.totalConnections}>
+                Total de 200 conexões já realizadas.{' '}
+                <Image source={heartIcon} />
+            </Text>
         </View>
-        <Text style={styles.totalConnections}>
-            Total de 200 conexões já realizadas.{' '}
-            <Image source={heartIcon}/>
-        </Text>
-    </View>
-    
+
     )
 }
 
--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from './StudyTabs';
 
 const { Navigator, Screen } = createStackNavigator();
 function AppStack() {
@@ -12,6 +13,7 @@ function AppStack() {
             <Navigator screenOptions={{headerShown: false}}>
                 <Screen name="Landing" component={Landing}/>
                 <Screen name="GiveClasses" component={GiveClasses}/>
+                <Screen name="Study" component={StudyTabs}/>
             </Navigator>
         </NavigationContainer>
     )
